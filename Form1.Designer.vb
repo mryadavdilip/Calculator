9c6646808c4b4dafb280e1cfde5e110074a47c40
Dilip Yadav <mryadavdilip@gmail.com>
2022-06-23 14:11:39 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,375 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn_negative = New System.Windows.Forms.Button()
+        Me.btn_num_zero = New System.Windows.Forms.Button()
+        Me.btn_decimal_point = New System.Windows.Forms.Button()
+        Me.btn_equal = New System.Windows.Forms.Button()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.btn_num_nine = New System.Windows.Forms.Button()
+        Me.btn_num_eight = New System.Windows.Forms.Button()
+        Me.btn_num_seven = New System.Windows.Forms.Button()
+        Me.btn_subtract = New System.Windows.Forms.Button()
+        Me.btn_num_six = New System.Windows.Forms.Button()
+        Me.btn_num_five = New System.Windows.Forms.Button()
+        Me.btn_num_four = New System.Windows.Forms.Button()
+        Me.btn_mul = New System.Windows.Forms.Button()
+        Me.btn_num_three = New System.Windows.Forms.Button()
+        Me.btn_num_two = New System.Windows.Forms.Button()
+        Me.btn_num_one = New System.Windows.Forms.Button()
+        Me.btn_divide = New System.Windows.Forms.Button()
+        Me.btn_sqrt = New System.Windows.Forms.Button()
+        Me.btn_sqr = New System.Windows.Forms.Button()
+        Me.btn_inverse = New System.Windows.Forms.Button()
+        Me.btn_del = New System.Windows.Forms.Button()
+        Me.btn_c = New System.Windows.Forms.Button()
+        Me.btn_ce = New System.Windows.Forms.Button()
+        Me.input_equation = New System.Windows.Forms.Label()
+        Me.input_value = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn_negative
+        '
+        Me.btn_negative.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_negative.Location = New System.Drawing.Point(12, 525)
+        Me.btn_negative.Name = "btn_negative"
+        Me.btn_negative.Size = New System.Drawing.Size(95, 56)
+        Me.btn_negative.TabIndex = 0
+        Me.btn_negative.Text = "±"
+        Me.btn_negative.UseVisualStyleBackColor = True
+        '
+        'btn_num_zero
+        '
+        Me.btn_num_zero.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_zero.Location = New System.Drawing.Point(113, 525)
+        Me.btn_num_zero.Name = "btn_num_zero"
+        Me.btn_num_zero.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_zero.TabIndex = 1
+        Me.btn_num_zero.Text = "0"
+        Me.btn_num_zero.UseVisualStyleBackColor = True
+        '
+        'btn_decimal_point
+        '
+        Me.btn_decimal_point.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_decimal_point.Location = New System.Drawing.Point(214, 525)
+        Me.btn_decimal_point.Name = "btn_decimal_point"
+        Me.btn_decimal_point.Size = New System.Drawing.Size(95, 56)
+        Me.btn_decimal_point.TabIndex = 2
+        Me.btn_decimal_point.Text = "."
+        Me.btn_decimal_point.UseVisualStyleBackColor = True
+        '
+        'btn_equal
+        '
+        Me.btn_equal.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_equal.Location = New System.Drawing.Point(315, 525)
+        Me.btn_equal.Name = "btn_equal"
+        Me.btn_equal.Size = New System.Drawing.Size(95, 56)
+        Me.btn_equal.TabIndex = 3
+        Me.btn_equal.Text = "="
+        Me.btn_equal.UseVisualStyleBackColor = True
+        '
+        'btn_add
+        '
+        Me.btn_add.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_add.Location = New System.Drawing.Point(315, 463)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(95, 56)
+        Me.btn_add.TabIndex = 8
+        Me.btn_add.Text = "+"
+        Me.btn_add.UseVisualStyleBackColor = True
+        '
+        'btn_num_nine
+        '
+        Me.btn_num_nine.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_nine.Location = New System.Drawing.Point(214, 463)
+        Me.btn_num_nine.Name = "btn_num_nine"
+        Me.btn_num_nine.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_nine.TabIndex = 7
+        Me.btn_num_nine.Text = "9"
+        Me.btn_num_nine.UseVisualStyleBackColor = True
+        '
+        'btn_num_eight
+        '
+        Me.btn_num_eight.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_eight.Location = New System.Drawing.Point(113, 463)
+        Me.btn_num_eight.Name = "btn_num_eight"
+        Me.btn_num_eight.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_eight.TabIndex = 6
+        Me.btn_num_eight.Text = "8"
+        Me.btn_num_eight.UseVisualStyleBackColor = True
+        '
+        'btn_num_seven
+        '
+        Me.btn_num_seven.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_seven.Location = New System.Drawing.Point(12, 463)
+        Me.btn_num_seven.Name = "btn_num_seven"
+        Me.btn_num_seven.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_seven.TabIndex = 5
+        Me.btn_num_seven.Text = "7"
+        Me.btn_num_seven.UseVisualStyleBackColor = True
+        '
+        'btn_subtract
+        '
+        Me.btn_subtract.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_subtract.Location = New System.Drawing.Point(315, 401)
+        Me.btn_subtract.Name = "btn_subtract"
+        Me.btn_subtract.Size = New System.Drawing.Size(95, 56)
+        Me.btn_subtract.TabIndex = 13
+        Me.btn_subtract.Text = "-"
+        Me.btn_subtract.UseVisualStyleBackColor = True
+        '
+        'btn_num_six
+        '
+        Me.btn_num_six.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_six.Location = New System.Drawing.Point(214, 401)
+        Me.btn_num_six.Name = "btn_num_six"
+        Me.btn_num_six.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_six.TabIndex = 12
+        Me.btn_num_six.Text = "6"
+        Me.btn_num_six.UseVisualStyleBackColor = True
+        '
+        'btn_num_five
+        '
+        Me.btn_num_five.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_five.Location = New System.Drawing.Point(113, 401)
+        Me.btn_num_five.Name = "btn_num_five"
+        Me.btn_num_five.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_five.TabIndex = 11
+        Me.btn_num_five.Text = "5"
+        Me.btn_num_five.UseVisualStyleBackColor = True
+        '
+        'btn_num_four
+        '
+        Me.btn_num_four.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_four.Location = New System.Drawing.Point(12, 401)
+        Me.btn_num_four.Name = "btn_num_four"
+        Me.btn_num_four.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_four.TabIndex = 10
+        Me.btn_num_four.Text = "4"
+        Me.btn_num_four.UseVisualStyleBackColor = True
+        '
+        'btn_mul
+        '
+        Me.btn_mul.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_mul.Location = New System.Drawing.Point(315, 339)
+        Me.btn_mul.Name = "btn_mul"
+        Me.btn_mul.Size = New System.Drawing.Size(95, 56)
+        Me.btn_mul.TabIndex = 18
+        Me.btn_mul.Text = "×"
+        Me.btn_mul.UseVisualStyleBackColor = True
+        '
+        'btn_num_three
+        '
+        Me.btn_num_three.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_three.Location = New System.Drawing.Point(214, 339)
+        Me.btn_num_three.Name = "btn_num_three"
+        Me.btn_num_three.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_three.TabIndex = 17
+        Me.btn_num_three.Text = "3"
+        Me.btn_num_three.UseVisualStyleBackColor = True
+        '
+        'btn_num_two
+        '
+        Me.btn_num_two.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_two.Location = New System.Drawing.Point(113, 339)
+        Me.btn_num_two.Name = "btn_num_two"
+        Me.btn_num_two.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_two.TabIndex = 16
+        Me.btn_num_two.Text = "2"
+        Me.btn_num_two.UseVisualStyleBackColor = True
+        '
+        'btn_num_one
+        '
+        Me.btn_num_one.Font = New System.Drawing.Font("Segoe UI Semibold", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn_num_one.Location = New System.Drawing.Point(12, 339)
+        Me.btn_num_one.Name = "btn_num_one"
+        Me.btn_num_one.Size = New System.Drawing.Size(95, 56)
+        Me.btn_num_one.TabIndex = 15
+        Me.btn_num_one.Text = "1"
+        Me.btn_num_one.UseVisualStyleBackColor = True
+        '
+        'btn_divide
+        '
+        Me.btn_divide.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_divide.Location = New System.Drawing.Point(315, 277)
+        Me.btn_divide.Name = "btn_divide"
+        Me.btn_divide.Size = New System.Drawing.Size(95, 56)
+        Me.btn_divide.TabIndex = 22
+        Me.btn_divide.Text = "÷"
+        Me.btn_divide.UseVisualStyleBackColor = True
+        '
+        'btn_sqrt
+        '
+        Me.btn_sqrt.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_sqrt.Location = New System.Drawing.Point(214, 277)
+        Me.btn_sqrt.Name = "btn_sqrt"
+        Me.btn_sqrt.Size = New System.Drawing.Size(95, 56)
+        Me.btn_sqrt.TabIndex = 21
+        Me.btn_sqrt.Text = "√x"
+        Me.btn_sqrt.UseVisualStyleBackColor = True
+        '
+        'btn_sqr
+        '
+        Me.btn_sqr.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_sqr.Location = New System.Drawing.Point(113, 277)
+        Me.btn_sqr.Name = "btn_sqr"
+        Me.btn_sqr.Size = New System.Drawing.Size(95, 56)
+        Me.btn_sqr.TabIndex = 20
+        Me.btn_sqr.Text = "x²"
+        Me.btn_sqr.UseVisualStyleBackColor = True
+        '
+        'btn_inverse
+        '
+        Me.btn_inverse.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_inverse.Location = New System.Drawing.Point(12, 277)
+        Me.btn_inverse.Name = "btn_inverse"
+        Me.btn_inverse.Size = New System.Drawing.Size(95, 56)
+        Me.btn_inverse.TabIndex = 19
+        Me.btn_inverse.Text = "⅟x"
+        Me.btn_inverse.UseVisualStyleBackColor = True
+        '
+        'btn_del
+        '
+        Me.btn_del.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_del.Location = New System.Drawing.Point(284, 215)
+        Me.btn_del.Name = "btn_del"
+        Me.btn_del.Size = New System.Drawing.Size(126, 56)
+        Me.btn_del.TabIndex = 26
+        Me.btn_del.Text = "⌫"
+        Me.btn_del.UseVisualStyleBackColor = True
+        '
+        'btn_c
+        '
+        Me.btn_c.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_c.Location = New System.Drawing.Point(144, 215)
+        Me.btn_c.Name = "btn_c"
+        Me.btn_c.Size = New System.Drawing.Size(134, 56)
+        Me.btn_c.TabIndex = 25
+        Me.btn_c.Text = "C"
+        Me.btn_c.UseVisualStyleBackColor = True
+        '
+        'btn_ce
+        '
+        Me.btn_ce.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_ce.Location = New System.Drawing.Point(12, 215)
+        Me.btn_ce.Name = "btn_ce"
+        Me.btn_ce.Size = New System.Drawing.Size(126, 56)
+        Me.btn_ce.TabIndex = 24
+        Me.btn_ce.Text = "CE"
+        Me.btn_ce.UseVisualStyleBackColor = True
+        '
+        'input_equation
+        '
+        Me.input_equation.AutoSize = True
+        Me.input_equation.CausesValidation = False
+        Me.input_equation.Location = New System.Drawing.Point(12, 76)
+        Me.input_equation.Name = "input_equation"
+        Me.input_equation.Size = New System.Drawing.Size(0, 25)
+        Me.input_equation.TabIndex = 28
+        Me.input_equation.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'input_value
+        '
+        Me.input_value.BackColor = System.Drawing.SystemColors.Control
+        Me.input_value.Enabled = False
+        Me.input_value.Font = New System.Drawing.Font("Segoe UI Semibold", 26.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.input_value.Location = New System.Drawing.Point(12, 115)
+        Me.input_value.MaxLength = 14
+        Me.input_value.Name = "input_value"
+        Me.input_value.ReadOnly = True
+        Me.input_value.Size = New System.Drawing.Size(398, 77)
+        Me.input_value.TabIndex = 29
+        Me.input_value.Text = "0"
+        Me.input_value.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 594)
+        Me.Controls.Add(Me.input_value)
+        Me.Controls.Add(Me.input_equation)
+        Me.Controls.Add(Me.btn_del)
+        Me.Controls.Add(Me.btn_c)
+        Me.Controls.Add(Me.btn_ce)
+        Me.Controls.Add(Me.btn_divide)
+        Me.Controls.Add(Me.btn_sqrt)
+        Me.Controls.Add(Me.btn_sqr)
+        Me.Controls.Add(Me.btn_inverse)
+        Me.Controls.Add(Me.btn_mul)
+        Me.Controls.Add(Me.btn_num_three)
+        Me.Controls.Add(Me.btn_num_two)
+        Me.Controls.Add(Me.btn_num_one)
+        Me.Controls.Add(Me.btn_subtract)
+        Me.Controls.Add(Me.btn_num_six)
+        Me.Controls.Add(Me.btn_num_five)
+        Me.Controls.Add(Me.btn_num_four)
+        Me.Controls.Add(Me.btn_add)
+        Me.Controls.Add(Me.btn_num_nine)
+        Me.Controls.Add(Me.btn_num_eight)
+        Me.Controls.Add(Me.btn_num_seven)
+        Me.Controls.Add(Me.btn_equal)
+        Me.Controls.Add(Me.btn_decimal_point)
+        Me.Controls.Add(Me.btn_num_zero)
+        Me.Controls.Add(Me.btn_negative)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(444, 650)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(444, 650)
+        Me.Name = "Form1"
+        Me.Opacity = 0.95R
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_negative As Button
+    Friend WithEvents btn_num_zero As Button
+    Friend WithEvents btn_decimal_point As Button
+    Friend WithEvents btn_equal As Button
+    Friend WithEvents btn_add As Button
+    Friend WithEvents btn_num_nine As Button
+    Friend WithEvents btn_num_eight As Button
+    Friend WithEvents btn_num_seven As Button
+    Friend WithEvents btn_subtract As Button
+    Friend WithEvents btn_num_six As Button
+    Friend WithEvents btn_num_five As Button
+    Friend WithEvents btn_num_four As Button
+    Friend WithEvents btn_mul As Button
+    Friend WithEvents btn_num_three As Button
+    Friend WithEvents btn_num_two As Button
+    Friend WithEvents btn_num_one As Button
+    Friend WithEvents btn_divide As Button
+    Friend WithEvents btn_sqrt As Button
+    Friend WithEvents btn_sqr As Button
+    Friend WithEvents btn_inverse As Button
+    Friend WithEvents btn_del As Button
+    Friend WithEvents btn_c As Button
+    Friend WithEvents btn_ce As Button
+    Friend WithEvents input_equation As Label
+    Friend WithEvents input_value As TextBox
+End Class
